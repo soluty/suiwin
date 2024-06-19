08e156f27d5056f7efbca9e275e83d8ab3ce9b25
soluty <soluty.lv@qq.com>
2024-06-19 17:32:43 +0800

    用env来获取web3form的token

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import icon from "astro-icon";
+// import { loadEnv } from 'vite';
+// const env = loadEnv(process.env.NODE_ENV, process.cwd(), 'SUIWIN');
 
 // https://astro.build/config
 export default defineConfig({
--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,5 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+interface SuiEnv {
+  readonly SUIWIN_FORM_TOKEN: string;
+}
